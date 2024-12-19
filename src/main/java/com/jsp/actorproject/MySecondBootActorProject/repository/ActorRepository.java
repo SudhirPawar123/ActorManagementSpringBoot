07b71bb64c616e2fdce4461f0f49d37d7a330090
SudhirPawar123 <sudhipawar329@gmail.com>
2024-12-20 00:19:07 +0530

    added dependency

--- src/main/java/com/jsp/actorproject/MySecondBootActorProject/repository/ActorRepository.java
+++ src/main/java/com/jsp/actorproject/MySecondBootActorProject/repository/ActorRepository.java
@@ -4,9 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jsp.actorproject.MySecondBootActorProject.model.Actor;
-import com.jsp.springboot.MyFirstBootProject.model.Student;
 
 @Repository
-public interface ActorRepository extends JpaRepository<Actor, Integer> {
+public interface ActorRepository extends JpaRepository<Actor,Integer> {
 
 }
